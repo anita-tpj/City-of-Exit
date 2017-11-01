cd84ad9bf53830ae520266e56bf64cdc506f44de
Anita Tapaji <anita.tpj@gmail.com>
2017-11-01 14:45:51 +0100

    Initial commit

---
+++ scripts/scrolling.js
@@ -0,0 +1,42 @@
+
+$(function () {
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
+    });
+
+  // add Bootstrap's scrollspy
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: 160
+    });
+
+    // smooth scrolling
+    $('nav a, .down-button a').bind('click', function () {
+        $('html, body').stop().animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+
+    // initialize WOW for elements animation
+    new WOW().init();
+  
+
+ /* 
+
+    // img showing/hidding
+if (window.innerWidth > 700){
+   $(".repeat-text").on({
+        mouseenter: function(){
+          $("#repeat-img").show();
+        }
+       // mouseleave: function(){
+       //   $("#repeat-img").hide();
+       // }
+});
+};
+    */
+     // parallax scroling with stellar.js
+    $(window).stellar();
+});
